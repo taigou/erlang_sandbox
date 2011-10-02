0463358dde79cc63bdca9b3f4f02ddc605182351
Hirotake Fuchigami <taigou@fuchigami>
2011-10-03 08:40:40 +0900

    exercise6-1 first commit

---
+++ chapter6/echo_server.erl
@@ -0,0 +1,24 @@
+-module(echo_server).
+-export([start/0, print/1, stop/1, loop/0]).
+
+start() ->
+  process_flag(trap_exit, true),
+  Pid = spawn_link(?MODULE, loop, []),
+  register(echo_server, Pid),
+  { ok, Pid }.
+
+print(Term) ->
+  echo_server ! { print, Term }.
+
+stop(Pid) ->
+%%  echo_server ! stop.
+  exit(Pid).
+
+loop() ->
+  receive
+    { print, Term } ->
+      io:format("Only use here ~p variable~n", [Term]),
+      loop()
+%%    stop ->
+%%      ok
+  end.
